ff922ce0a0c4f23fb4039c587b37aca5dceb46a4
kirankumar-h <kirankumarak1630@gmail.com>
2023-02-15 13:22:12 +0530

    second attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,14 +5,14 @@ public class Main {
     public static class Product{
 
        public int product(int x, int y){
-           return x;
+           return x*y;
        }
 
         public int product(int x, int y, int z) {
-           return x;
+           return x*y*z;
         }
        public double product(double x, double y){
-           return x;
+           return x*y;
        }
     }
 
